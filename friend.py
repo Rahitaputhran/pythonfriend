8684daed1cd88e9418973a6662d7e540699d1baa
Rahitaputhran <puthranrahita@gmail.com>
2025-02-17 13:08:00 +0530

    texttospeech.py

--- friend.py
+++ friend.py
@@ -6,4 +6,10 @@ friend.say("hello everyone. Rahita here!")
 #taking the input from the user
 speech=input("enter the input: ")
 friend.say(speech)
+
+#change the speed of the speech
+rate=friend.getProperty('rate')
+print(f"the rate is {rate}")#by default it is 200
+friend.setProperty('rate',130)
+friend.say("hello! the speed is 150")
 friend.runAndWait()
